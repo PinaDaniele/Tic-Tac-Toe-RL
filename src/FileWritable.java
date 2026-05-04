e57505b268fe13cfc2259d9f8ff81e6b4aef0cec
PinaDaniele <pinazdaniele@gmail.com>
2026-05-04 19:55:11 +0200

    Added file saving for QTable (as CSV)

---
+++ src/FileWritable.java
@@ -0,0 +1,6 @@
+public interface FileWritable {
+
+    public abstract void saveToFile(String fileName);
+
+    public abstract void loadFromFile(String fileName);
+}
--- src/QTable.java
+++ src/QTable.java
@@ -1,12 +1,19 @@
 import java.util.HashMap;
+import java.io.*;
 
-public class QTable {
+public class QTable implements FileWritable {
     private HashMap<String, double[]> table;
     private static int numActions;
 
     public QTable(int actions){
         numActions = actions;
         table = new HashMap<>();
+    }
+
+    public QTable(int actions, String fileName){
+        numActions = actions;
+        table = new HashMap<>();
+        loadFromFile(fileName);
     }
 
     public double[] getQValues(String state){
@@ -31,6 +38,63 @@ public class QTable {
             }
         }
         return maxQValue;
+    }
+
+    @Override
+    public void saveToFile(String fileName){
+        File file = new File(fileName);
+
+        try(PrintWriter writer = new PrintWriter(file)){
+            for(String state: table.keySet()){
+                double[] QValues = table.get(state);
+                String line = state;
+
+                for(double QValue: QValues){
+                    line += "," + QValue;
+                }
+                writer.println(line);
+            }
+        }
+        catch(IOException e){
+            System.err.println("An error occured while saving the QTable: " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void loadFromFile(String fileName){
+        table.clear();
+        FileReader file;
+        try{
+            file = new FileReader(fileName);
+        }
+        catch (FileNotFoundException e){
+            System.err.println("File not found: " + e.getMessage());
+            return;
+        }
+        
+
+        try(BufferedReader reader = new BufferedReader(file)){
+            String line = reader.readLine();
+
+            while(line != null){
+                String[] parts = line.split(",");
+                String state = parts[0];
+                double[] QValues = new double[numActions];
+
+                for(int i=0; i<numActions; i++){
+                    QValues[i] = Double.parseDouble(parts[i+1]);
+                }
+
+                table.put(state, QValues);
+                line = reader.readLine();
+            }
+
+        }
+        catch(IOException e){
+            System.err.println("An error occured while loading the QTable: " + e.getMessage());
+        }
+
     }
 
 }
